9bc192f5045305beea45d8b07cbd685555f5e31f
Andres Mena <133726719+DAMena96@users.noreply.github.com>
2023-06-06 12:54:17 -0500

    Add files via upload

---
+++ Arreglos.js
@@ -0,0 +1,31 @@
+//Arreglos
+// EJERCICIO 1
+/*
+let numeros = [];
+for (let i = 0; i < 10; i++) {
+  let numeroAleatorio = Math.floor(Math.random() * 100);
+  numeros.push(numeroAleatorio);
+}
+console.log(numeros);
+*/
+
+// EJERCICIO 2
+/*
+let input = prompt("Ingrese palabras separadas por coma");
+let palabras = input.split(",");
+console.log(palabras);
+*/
+
+// EJERCICIO 3
+/*
+let numeros = [10, 40, 30, 20, 15, 5];
+let numerosOrdenados = numeros.sort(function(a, b) {
+  return a - b;
+});
+let numeroMenor = Math.min(...numeros);
+let numeroMayor = Math.max(...numeros);
+console.log("Arreglo ordenado de menor a mayor:", numerosOrdenados);
+console.log("Número menor:", numeroMenor);
+console.log("Número mayor:", numeroMayor);
+*/
+
